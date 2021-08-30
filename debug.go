4d932294f0e3149c70bef62b8760f8b0aa3b027f
larryhou <larryhou@tencent.com>
2021-08-30 16:48:51 +0800

    Add receive/send buffer size info for PrintSockopts

--- debug.go
+++ debug.go
@@ -6,6 +6,8 @@ import (
 )
 
 func PrintSockopts(fd int) {
+    if v, err := syscall.GetsockoptInt(fd, syscall.SOL_SOCKET, syscall.SO_RCVBUF); err == nil {fmt.Printf("# %d::RCVBUF=%.1fKB\n", fd, float64(v)/1024)}
+    if v, err := syscall.GetsockoptInt(fd, syscall.SOL_SOCKET, syscall.SO_SNDBUF); err == nil {fmt.Printf("# %d::SNDBUF=%.1fKB\n", fd, float64(v)/1024)}
     if v, err := syscall.GetsockoptInt(fd, syscall.IPPROTO_TCP, 0x001); err == nil {fmt.Printf("# %d::TCP_NODELAY=%X\n", fd, v)}
     if v, err := syscall.GetsockoptInt(fd, syscall.IPPROTO_TCP, 0x103); err == nil {fmt.Printf("# %d::TCP_SENDMOREACKS=%X\n", fd, v)}
     if v, err := syscall.GetsockoptInt(fd, syscall.IPPROTO_TCP, 0x004); err == nil {fmt.Printf("# %d::TCP_NOPUSH=%X\n", fd, v)}
